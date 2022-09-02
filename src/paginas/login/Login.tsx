6230d20c96847d0840a33468e704dcd9565894e3
Jonathan <jonathancqaa@gmail.com>
2022-09-02 15:00:58 -0300

    @jonathancqa [Task: Componente de Login completo] {02/08/2022 15:00}

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,4 +1,5 @@
-import { Grid, TextField, Typography } from '@material-ui/core';
+import { Grid, TextField, Typography, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import { TextFields } from '@mui/icons-material';
 import { Box } from '@mui/material';
 import React from 'react';
@@ -6,17 +7,35 @@ import './Login.css';
 
 function Login(){
     return(
-        <Grid container>
-            <Grid xs={6}>
-                <Box>
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid alignItems='center' xs={6}>
+                <Box paddingX={20}>
                     <form>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight: 'bold'}}>Entrar</Typography>
                         <TextField id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
                         <TextField id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                            <Link to='/home' className='text-decorator-none'>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Logar
+                                </Button>
+                            </Link>
+                        </Box>
                     </form>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
+                        </Box>
+                        <Typography variant='subtitle1' gutterBottom align='center' style={{ fontWeight:'bold' }}>Cadastre-se</Typography>
+                    </Box>
                 </Box>
             </Grid>
-            <Grid xs={6}>
+            <Grid xs={6} style={{
+                backgroundImage: `url(https://i.imgur.com/d5bMdDJ.jpg)`,
+                backgroundRepeat: 'no-repeat', width: '100vh', minHeight: '100vh', 
+                backgroundSize: 'cover', 
+                backgroundPosition: 'center'
+            }}>
 
             </Grid>
         </Grid>
