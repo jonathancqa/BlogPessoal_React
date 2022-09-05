da9e16eb1282b4bb3ce927d6ed94e62aece4723b
Jonathan <jonathancqaa@gmail.com>
2022-09-05 15:16:20 -0300

    @jonathancqa [Task: Criado componente de postagens e temas] {05/09/2022 15:16}

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,27 @@ import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Login from './paginas/login/Login';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 import './App.css';
 
 function App() {
-  return(
-   <Router>
+  return (
+    <Router>
       <Navbar />
-        <div style={{minHeight: '100vh'}}>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
-          </Routes>
-        </div>
+      <div style={{ minHeight: '100vh' }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
+        </Routes>
+      </div>
       <Footer />
-   </Router>
-    );
+    </Router>
+  );
 }
 
 export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Grid, Typography } from '@material-ui/core';
 import { Box } from "@mui/material";
 import './Home.css';
+import TabPostagem from "../../components/postagens/tabpostagem/TabPostagem";
 
 function Home() {
   return (
@@ -22,6 +23,7 @@ function Home() {
           <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
         </Grid>
         <Grid xs={12} className='postagens'>
+          <TabPostagem />
         </Grid>
       </Grid>
     </>
