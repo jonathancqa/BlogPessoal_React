1aed4a6babd72baeb7aa5c778affa3fbcd7e9846
Jonathan <jonathancqaa@gmail.com>
2022-09-12 09:51:55 -0300

    @jonathancqa [Task: Comunicação com Back-End de Tema e Postagens] {12/09/2022 09:51}

--- src/components/postagens/listapostagem/ListaPostagem.tsx
+++ src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -1,51 +1,81 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
 import './ListaPostagem.css';
+import Postagem from '../../../models/Postagem';
+import useLocalStorage from 'react-use-localstorage';
+import { busca } from '../../../services/Services';
 
 function ListaPostagem() {
+    const [posts, setPosts] = useState<Postagem[]>([])
+    const [token, setToken] = useLocalStorage('token');
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        if (token == '') {
+            alert("Você precisa estar logado")
+            navigate("/login")
+        }
+    }, [token])
+
+    async function getPosts() {
+        await busca("/postagens", setPosts, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [posts.length])
 
     return (
         <>
-            <Box m={2} >
-                <Card variant="outlined">
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>
-                            Postagens
-                        </Typography>
-                        <Typography variant="h5" component="h2">
-                            Título
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            Texto da Postagem
-                        </Typography>
-                        <Typography variant="body2" component="p">
-                            Tema
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Box display="flex" justifyContent="center" mb={1.5}>
+            {
+                posts.map(posts => (
+                    <Box m={2} >
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    Postagens
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                    {posts.titulo}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {posts.texto}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {posts.tema?.descricao}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Box display="flex" justifyContent="center" mb={1.5}>
 
-                            <Link to="" className="text-decorator-none" >
-                                <Box mx={1}>
-                                    <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                                        atualizar
-                                    </Button>
+                                    <Link to={`/formularioPostagem/${posts.id}`} className="text-decorator-none" >
+                                        <Box mx={1}>
+                                            <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                                                atualizar
+                                            </Button>
+                                        </Box>
+                                    </Link>
+                                    <Link to={`deletarPostagem/${posts.id}`} className="text-decorator-none">
+                                        <Box mx={1}>
+                                            <Button variant="contained" size='small' color="secondary">
+                                                deletar
+                                            </Button>
+                                        </Box>
+                                    </Link>
                                 </Box>
-                            </Link>
-                            <Link to="" className="text-decorator-none">
-                                <Box mx={1}>
-                                    <Button variant="contained" size='small' color="secondary">
-                                        deletar
-                                    </Button>
-                                </Box>
-                            </Link>
-                        </Box>
-                    </CardActions>
-                </Card>
-            </Box>
-        </>)
+                            </CardActions>
+                        </Card>
+                    </Box>
+                ))
+            }
+        </>
+    )
 }
 
 export default ListaPostagem;
--- src/services/Services.ts
+++ src/services/Services.ts
@@ -4,12 +4,20 @@ export const api = axios.create({
     baseURL:'https://bloggeneration.herokuapp.com'
 })
 
+/* Método de Cadastro de Uusario */
 export const cadastroUsuario = async(url: any,dados: any,setDado: any) => {
     const resposta = await api.post(url,dados)
     setDado(resposta.data)
 }
 
+/*Método de Login de Usuario */
 export const login = async(url: any,dados: any,setDado: any) => {
     const resposta = await api.post(url,dados)
     setDado(resposta.data.token)
+}
+
+/*Método para listar/buscar e postagens/temas */
+export const busca = async(url: any,setDado: any, header: any) => {
+    const resposta = await api.get(url,header)
+    setDado(resposta.data)
 }
