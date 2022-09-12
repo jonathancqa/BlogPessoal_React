66dcc0ade0be78cd1e5bc77f5ad280f117604847
Jonathan <jonathancqaa@gmail.com>
2022-09-12 11:07:23 -0300

    @jonathancqa [Task: CRUD do componente Tema completo] {12/09/2022 11:07}

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -21,3 +21,26 @@ export const busca = async(url: any,setDado: any, header: any) => {
     const resposta = await api.get(url,header)
     setDado(resposta.data)
 }
+
+/*Método para listar/buscar e postagens/temas pelo Id */
+export const buscaId = async(url: any,setDado: any, header: any) => {
+    const resposta = await api.get(url,header)
+    setDado(resposta.data)
+}
+
+/*Método para criar e postagens/temas */
+export const post = async(url: any, dados:any, setDado: any, header: any) => {
+    const resposta = await api.post(url,dados,header)
+    setDado(resposta.data)
+}
+
+/*Método para atualizar postagens/temas */
+export const put = async(url: any, dados:any, setDado: any, header: any) => {
+    const resposta = await api.put(url,dados,header)
+    setDado(resposta.data)
+}
+
+/*Método para deletar postagens e temas */
+export const deleteId = async(url: any, header: any) => {
+    await api.delete(url,header)
+}
